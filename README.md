6d9c1e80cca84a24762f2ac58495017eda7ac173
cyu9 <calvinyu.purdue@gmail.com>
2022-03-03 09:35:14 -0800

    update example script and readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Code from ["Towards deep learning with segregated dendrites" by Jordan Guergiuev
 See `example_script.py` for an example of loading MNIST, creating a network and training it.
 
 This code requires the [MNIST dataset files](http://yann.lecun.com/exdb/mnist/) to be downloaded and placed in the same directory as the code.
+
+Instructions:
+- use Python 3.7
+- install numpy, matplotlib, scipy
--- example_script.py
+++ example_script.py
@@ -25,8 +25,8 @@ dl.use_sparse_feedback = True
 # set training parameters
 f_etas = (0.21, 0.21)
 b_etas = None
-n_epochs = 10
-n_training_examples = 600
+n_epochs = 5
+n_training_examples = 1000 # use at least 1000 examples!
 
 # create the network -- this will also load the MNIST dataset files
 net = dl.Network(n=(500, 10))
@@ -35,7 +35,7 @@ net = dl.Network(n=(500, 10))
 net.train(f_etas, b_etas, n_epochs, n_training_examples, save_simulation=True, simulations_folder="Simulations", folder_name="Example Simulation")
 
 # re-load the saved simulation & network
-net, f_etas, b_etas, n_training_examples = dl.load_simulation(latest_epoch=9, folder_name="Example Simulation", simulations_folder="Simulations")
+net, f_etas, b_etas, n_training_examples = dl.load_simulation(latest_epoch=n_epochs-1, folder_name="Example Simulation", simulations_folder="Simulations")
 
-# train the network for another 10 epochs
+# train the network for another n_epochs
 net.train(f_etas, b_etas, n_epochs, n_training_examples, save_simulation=True, simulations_folder="Simulations", folder_name="Example Simulation")
