f80aeac8b707aba2ee74143da06ef428c1c5a848
Calvin Yu <cyu@stellartec.com>
2022-03-22 21:29:26 -0700

    update script with layer details

--- example_script.py
+++ example_script.py
@@ -23,13 +23,13 @@ import deep_learning as dl
 dl.use_sparse_feedback = True
 
 # set training parameters
-f_etas = (0.21, 0.21)
+f_etas = (0.21, 0.21, 0.21, 0.21)
 b_etas = None
 n_epochs = 5
 n_training_examples = 1000 # use at least 1000 examples!
 
 # create the network -- this will also load the MNIST dataset files
-net = dl.Network(n=(500, 10))
+net = dl.Network(n=(784, 100, 100, 10))
 
 # train the network
 net.train(f_etas, b_etas, n_epochs, n_training_examples, save_simulation=True, simulations_folder="Simulations", folder_name="Example Simulation")
